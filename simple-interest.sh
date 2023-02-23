21cc97565e9987b7741a8970f3a39dc1307b9971
Sirichandanapendyala <113296402+Sirichandanapendyala@users.noreply.github.com>
2023-02-23 22:37:46 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Sirichandanapendyala
    # Additional Authors:
-   # <your GitHub username>
+   # <Sirichandanapendyala>
 
    # Input:
    # p, principal amount
